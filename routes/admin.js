9ad014bd6932f027f95b4b42654ac40ce4930314
Abhishek Gajera <abhishek@verityplatforms.com>
2025-04-25 16:43:15 +0530

    register commit

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controller/authController");
 const { authCheck } = require("../middleware/auth.middleware");
 
-router.route("/register").get(authController.getRegister).post(authController.register);
+// router.route("/register").get(authController.getRegister).post(authController.register);
 router.route("/login").get(authController.getLogin).post(authController.login);
 router.get("/logout", authController.logout);
 router.get("/profile", authCheck, authController.profileGet);
